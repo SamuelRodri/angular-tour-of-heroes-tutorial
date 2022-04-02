bf47ec4d69492dc9561ae19bc073282013bff1f0
SamuelRodri <samuelrh2001@hotmail.com>
2022-04-02 11:14:35 +0100

    2. Display a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Weird' },
+  { id: 12, name: 'Cockroach-Man' },
+  { id: 13, name: 'White Cougar' },
+  { id: 14, name: 'Bananhos' },
+  { id: 15, name: 'Crimson Sorceress' },
+  { id: 16, name: 'Dynamic Needle' },
+  { id: 17, name: 'Broot' },
+  { id: 18, name: 'Nagmeto' },
+  { id: 19, name: 'Humanoid Matchstick' },
+  { id: 20, name: 'Stan Read' }
+]
